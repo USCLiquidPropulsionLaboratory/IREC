f1611f549445abdaed08bc03ec013598ce6f37b2
rahulrughani <rughani@usc.edu>
2016-11-13 14:28:21 -0800

    Add files via upload

--- rocket_sizing.m
+++ rocket_sizing.m
@@ -30,23 +30,32 @@ A = 0.25*pi*d^2; % cross sectional area [m^2]
 
 Mp = 10;    % initial guess of propellant mass [kg]
 
+% create function handle for solver input (solver requires a function
+% with only one input variable, so need to specify other inputs in a 
+% function handle)
 f = @(x) rckeqn_solve(x,mdot,Ms_0,Ml,g,Isp,alpha,h,A,Cd);
 
+% solve for propellant mass required to achieve desired altitude
 Mp = fzero(f,Mp);
 
-I = Mp*g*Isp;
+I = Mp*g*Isp;   % compute total impulse [N-sec]
 
+% get mass values of rocket from converged propellant mass
 Ms = Ms_0 + alpha*Mp;
 M0 = Ms + Mp + Ml;
 Mb = Ms + Ml;
 
-a_max = T/Mb;
-g_max = a_max/g;
+% get max acceleration
+a_max = T/Mb;       % [m/s^2]
+g_max = a_max/g;    % [g's]
 
+% get rocket mass ratio
 R = M0/Mb;
 
+% compute burnout and apex properties with converged propellant mass
 [hb,ub,tb,h,t] = rckeqn(Mp,mdot,Ms_0,Ml,g,Isp,alpha,A,Cd);
 
+% write results into a table
 Results = [Mp;M0;Mb;a_max;g_max;R;tb;t;hb;h;ub;I];
 Rows = {'Propellant Mass';'Wet Mass';'Dry Mass';'Max Accel';'Max Gs';...
     'R';'burnout time';'apex time';'burnout alt';'apex alt';...
@@ -56,7 +65,11 @@ Units = {'kg';'kg';'kg';'m/s^2';'g';'';'sec';'sec';'m';'m';'m/s';'N-sec'};
 
 ResultsTable = table(Results,Units,'RowNames',Rows);
 
+% print out table
 ResultsTable
+
+% write table to file
+writetable(ResultsTable,'Results.txt','Delimiter','\t','WriteRowNames',true);
 
 
 
