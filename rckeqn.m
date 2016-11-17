fc7149c6e6b76f7457778b6dc905c77a175dfc0f
rahulrughani <rughani@usc.edu>
2016-11-16 18:13:37 -0800

    Add files via upload

--- rckeqn.m
+++ rckeqn.m
@@ -14,49 +14,47 @@ Cd = q(8);
 of_ratio = q(9);
 R_sp = q(10);
 mu = q(11);
-rho_p = q(12);
+rho_f = q(12);
 rho_ox = q(13);
 rho_tank = q(14);
 sig_tank = q(15);
 FS_tank = q(16);
-P_p = q(17);
+P_f = q(17);
 P_ox = q(18);
+d_tank = q(19);
 
 % calculate cross-sectional area of rocket
 A = 0.25*pi*d^2;
 
-Mox = of_ratio*Mp;  % oxidizer mass
+Mf = Mp/(1+of_ratio);   % fuel mass
+Mox = Mp - Mf;          % oxidizer mass
 
-Vp = Mp/rho_p;      % volume of propellant
+Vf = Mf/rho_f;      % volume of propellant
 Vox = Mox/rho_ox;   % volume of oxidizer
-
-d_tank = d - 0.02;  % set tank diameter limits
 
 % we are using cylindrical tanks with domed ends, so lets find the height
 % of the cylindrical portion of the tanks
-W_p = (Vp - pi*d_tank^3/6)/(pi*d_tank^2/4);  
+W_f = (Vf - pi*d_tank^3/6)/(pi*d_tank^2/4);  
 W_ox = (Vox - pi*d_tank^3/6)/(pi*d_tank^2/4);
 
 % determine total heigh of tanks
-h_tank_p = W_p + d_tank;
+h_tank_f = W_f + d_tank;
 h_tank_ox = W_ox + d_tank;
 
 % determine masses of tanks, assuming a safety factor on yield stress
-M_tank_p = pi*d_tank^2/2*(d_tank/2+W_p)*FS_tank*P_p*rho_tank/sig_tank;
-M_tank_ox = pi*d_tank^2/2*(d_tank/2+W_ox)*FS_tank*P_ox*rho_tank/sig_tank;
+M_tank_f = 0.5*pi*d_tank^2/2*(d_tank/2+W_f)*FS_tank*P_f*rho_tank/sig_tank;
+M_tank_ox = 0.5*pi*d_tank^2/2*(d_tank/2+W_ox)*FS_tank*P_ox*rho_tank/sig_tank;
 
-% Assume structural mass of rocket is the base structural mass, plus an
-% overhead as a fraction of propellant mass (accounts for oxidizer, fuel
-% and ox tankage, and fuselage)
-Ms = Ms_0 + M_tank_p + M_tank_ox;
+% Assume structural mass of rocket is the base structural mass, plus
+% tankage masses, and an overhead for the rest of the fuselage
+Ms = Ms_0 + M_tank_f + M_tank_ox + alpha*Mp;
 % Ms = Ms_0 + alpha*Mp*of_ratio;
 
 % calculate wet and dry masses of rocket
-M0 = Ms + Mp + Mox + Ml;
+M0 = Ms + Mp + Ml;
 Mb = Ms + Ml;
 
 %% Initialize Variables for Numerical Integration
-mass_prop = Mp+Mox; % get initial propellant mass
 mass = M0;      % get initial wet mass
 dt = 0.01;      % specify timestep [sec]
 t= 0;           % specify start time [sec]
@@ -68,7 +66,7 @@ alt = 0;        % specify initial altitude (launchpad) [m]
 % continue iterations while there is fuel in the tank
 % numerical integration uses differential form of the rocket equation with
 % drag force D=0.5*rho*u^2*A*Cd
-while mass_prop > 0
+while mass > Mb
     grav = mu/(R_sp+alt)^2; % calculate local gravity
     rho = rho_alt(alt);     % calculate local atmospheric density
     
@@ -76,7 +74,6 @@ while mass_prop > 0
     du = (g*Isp*mdot/mass - 0.5*rho*u^2*A*Cd/mass - grav)*dt;
     u = u + du;
     alt = alt + u*dt;
-    mass_prop = mass - Mb;
     t = t + dt;
 end
 
--- rocket_sizing.m
+++ rocket_sizing.m
@@ -20,16 +20,17 @@ Isp =   295;    %   Isp [sec]
 mdot =  1.5;    %   mass flow rate [kg/s]
 
 % Rocket Properties
-Ms_0 =  40;     %   fixed mass [kg]
-alpha = 0.3;    %   fuel mass overhead
+Ms_0 =  30;     %   fixed mass [kg]
+alpha = 0.2;    %   fuel mass overhead
 of_ratio = 1.8; %   Oxidizer to fuel mass ratio
-rho_p = 810;    %   propellant density [kg/m^3]
-P_p = 500;      %   Propellant pressure [Psi]
+rho_f = 810;    %   propellant density [kg/m^3]
+P_f = 500;      %   Propellant pressure [Psi]
 P_ox = 2000;    %   ox tank pressure [Psi]
+d = 0.20;       %   rocket diameter [m]
+d_tank = d - 0.02;  %   fuel and ox tank diameters [m]
 
 % Drag Properties    
-d = 0.15;       %   rocket diameter [m]
-Cd = 0.5;       %   drag coefficient 
+Cd = 0.3;       %   drag coefficient 
 
 % Physical Properties
 g = 9.8066;     %   gravitational accel at Earth's surface [m/s^2]
@@ -41,14 +42,18 @@ R_univ = 8.3144598;     %   Universal gas constant [J/mol-K]
 MM_ox = 31.9988e-3;     %   Molar mass of oxygen gas (O2) [kg/mol]
 T_amb = 298.15; %   Ambient temperature [K]
 
-rho_tank = 8000;%   Density of 304 SS [kg/m^3]
-sig_tank = 2.15e8;      %   Yield Stress of 304 SS [Pa]
-FS_tank = 1.2;  %   Safety factor on tank yielding
+% fuel and ox tank material properties [kg/m^3] & [Pa]
+rho_tank = 8000;        %   Density of 304 SS [kg/m^3]
+sig_tank = 500e6;       %   Yield Stress of 304 SS [Pa]
+rho_tank = 1800;        % Carbon Fiber
+sig_tank = 6370e6;      % Carbon Fiber
+
+FS_tank = 1.2;  %   Safety factor on tank failure
 
 
 %% Calculations
 
-P_p = P_p * 6894.75729;     % convert to Pascals
+P_f = P_f * 6894.75729;     % convert to Pascals
 P_ox = P_ox * 6894.75729;   % convert to Pascals
 rho_ox = P_ox/(R_univ/MM_ox)/T_amb; % get density of oxygen in tank
 
@@ -62,17 +67,36 @@ Mp = 10;    % initial guess of propellant mass [kg]
 % create function handle for solver input (solver requires a function
 % with only one input variable, so need to specify other inputs in a 
 % function handle)
-q = [mdot,Ms_0,Ml,g,Isp,alpha,d,Cd,of_ratio,R_sp,mu,rho_p,rho_ox,...
-    rho_tank,sig_tank,FS_tank,P_p,P_ox];
+q = [mdot,Ms_0,Ml,g,Isp,alpha,d,Cd,of_ratio,R_sp,mu,rho_f,rho_ox,...
+    rho_tank,sig_tank,FS_tank,P_f,P_ox,d_tank];
 f = @(x) rckeqn_solve(x,q,h);
 
 % solve for propellant mass required to achieve desired altitude
-Mp = fsolve(f,Mp);
+Mp = fzero(f,Mp);
 
 I = Mp*g*Isp;   % compute total impulse [N-sec]
 
+Mf = Mp/(1+of_ratio);   % fuel mass
+Mox = Mp - Mf;          % oxidizer mass
+
+Vf = Mp/rho_f;      % volume of propellant
+Vox = Mox/rho_ox;   % volume of oxidizer
+
+% we are using cylindrical tanks with domed ends, so lets find the height
+% of the cylindrical portion of the tanks
+W_f = (Vf - pi*d_tank^3/6)/(pi*d_tank^2/4);  
+W_ox = (Vox - pi*d_tank^3/6)/(pi*d_tank^2/4);
+
+% determine total heigh of tanks
+h_tank_f = W_f + d_tank;
+h_tank_ox = W_ox + d_tank;
+
+% determine masses of tanks, assuming a safety factor on yield stress
+M_tank_f = pi*d_tank^2/2*(d_tank/2+W_f)*FS_tank*P_f*rho_tank/sig_tank;
+M_tank_ox = pi*d_tank^2/2*(d_tank/2+W_ox)*FS_tank*P_ox*rho_tank/sig_tank;
+
 % get mass values of rocket from converged propellant mass
-Ms = Ms_0 + alpha*Mp;
+Ms = Ms_0 + M_tank_f + M_tank_ox + alpha*Mp;
 M0 = Ms + Mp + Ml;
 Mb = Ms + Ml;
 
@@ -101,6 +125,4 @@ ResultsTable
 
 % write table to file
 writetable(ResultsTable,'Results.txt','Delimiter','\t','WriteRowNames',true);
-
-
 
